e2b3c01de4108a0c3430aae468be92534cf63cfb
Amaury Labrada <labrada83@gmail.com>
2016-01-02 13:28:15 -0500

    Just Installed!

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+	protected $toTruncate=['lessons'];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	foreach ($this->toTruncate as $table) {
+    		DB::table($table)->truncate();
+    	}
+        
+        $this->call(LessonsTableSeeder::class);
+    }
+}
